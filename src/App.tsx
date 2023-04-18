7cfc64932bd154b93b9c891f274050039e9ca6c8
RaccoonDinh <20521060@gm.uit.edu.vn>
2023-04-18 22:04:47 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="text-dark-slate-gray">test test text</p>
+    </div>
+  );
+}
+
+export default App;
